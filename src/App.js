46b7d16743c29da85f9280243bbd35fefa452366
Munish Thakur <munish81198@gmail.com>
2019-08-25 02:19:28 +0530

    Fixing bugs

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends Component {
   loginHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('http://localhost:8080/auth/login', {
+    fetch('https://munish-rest.herokuapp.com/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -109,7 +109,7 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('http://localhost:8080/auth/signup', {
+    fetch('https://munish-rest.herokuapp.com/auth/signup', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
